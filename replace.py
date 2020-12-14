d1e2d0c4ae052985f17ee8705f0e1c5d504c74e7
Yogyata <yogyata278@gmail.com>
2020-12-14 09:44:58 +0530

    random no. code added

--- replace.py
+++ replace.py
@@ -1,5 +1,10 @@
+import random
+
+print("Your Lucky Number is",random.randrange(1,10))
+
 sen =input("Enter a sentence:")
 fw=input("Find What?")
 rw=input("Replace with:")
 print("Modified sentence:",sen.replace(fw,rw))   #exchange  of two word????????????
+
 
